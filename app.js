0466ecde91c0e22631c8d6cbae5edf529256e511
Romain <RomainFrechic@outlook.fr>
2016-10-24 15:10:23 +0200

    Step 0 fonctionnel (password ok), Step 1 en cours (meteo) - Laure

--- app.js
+++ app.js
@@ -1,27 +1,48 @@
 $("#btn").click(function(){
    console.log("Go Recupère le tableau");
-  
+
    $.ajax('http://192.168.1.21:3000/places')
    .done(ajaxDone)
    .fail(ajaxFail);
 
    function ajaxDone(response) {
-      console.log(response);
       var places = response.places;
-      console.log(places);
+
       for (var i = 0; i < places.length; i++) {
-         var place = places[i];
+         $("#list").append("<li>" + places[i].nom + "</li>")
+         
+         if (places[i].nom === "IoT Valley") {
+            var motDePasse = places[5].password;
+            $('body').append("mot de passe:" + motDePasse);
+         }
       }
-      if (place.nom === IoT Valley) {
-         var motDePasse = places[5].password;
-         $('body').append("mot de passe:" + motDePasse);
-      }
-      $('ul').append('<li>' + places.nom + '</li>');
-      
-   }
+
+   };
 
    function ajaxFail(response) {
-      console.log(response);
+      alert("fail");
+   };
+
+});
+
+// Pour Step 1 = météo Open Weather
+// Le bouton fonctionne, ça envoie bien la requête mais il faut trouver comment entrer l'API key
+
+$('#btnOpenWeather').on('click', function() {
+   console.log('le clic sur le bouton météo fonctionne');
+
+   $.ajax('http://api.openweathermap.org/data/2.5/weather?q={toulouse}')
+      .done(openWeatherDone)
+      .fail(openWeatherFail)
+
+
+   function openWeatherDone(response) {
+      console.log("success");
+      alert("ok");
+   };
+
+   function openWeatherFail() {
+      alert("fail");
    }
 
 });
